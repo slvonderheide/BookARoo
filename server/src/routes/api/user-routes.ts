3c145cebaaa15d2425dfe8fa7ff4a44e4905f7fe
slvonderheide <slvonderheide@gmail.com>
2025-03-31 12:02:59 -0400

    updated some server routes

--- server/src/routes/api/user-routes.ts
+++ server/src/routes/api/user-routes.ts
@@ -12,7 +12,7 @@ import {
 import { authenticateToken } from '../../services/auth.js';
 
 // put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser).put(authenticateToken, saveBook);
+router.route('/').post(createUser).put(authenticateToken, saveBook); //get all users change with mutations
 
 router.route('/login').post(login);
 
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -8,9 +8,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import apiRoutes from './api/index.js';
 
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 
-// serve up react front-end in production
+// serve up react front-end in production modify this sever to look like the mini project
 router.use((_req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
